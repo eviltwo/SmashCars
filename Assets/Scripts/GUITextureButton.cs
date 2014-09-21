3c7d1eba2c5a167848b0ceb929cbb50f44f59db8
eviltwo <e135764@DaikiMacBookPro.local>
2014-09-21 14:36:23 +0900

    アイテムの色を変更
    白・中に黒を入れた

---
+++ Assets/Scripts/GUITextureButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUITextureButton : MonoBehaviour {
+	bool push = false;
+	void Update(){
+		if (!push) {
+			if (InputManager.Instance.getClick () == 1) {
+				Vector2 mousepos = InputManager.Instance.getMousePosMult ();
+				Vector2 mypos = transform.position;
+				Vector2 scl = transform.localScale;
+				if (mypos.x - scl.x / 2 < mousepos.x && mypos.x + scl.x / 2 > mousepos.x && mypos.y - scl.y / 2 < mousepos.y && mypos.y + scl.y / 2 > mousepos.y) {
+					SendMessage ("OnPushButton", SendMessageOptions.DontRequireReceiver);
+					push = true;
+				}
+			}
+		} else {
+			if (InputManager.Instance.getClick () == 0) {
+				SendMessage ("OnReleaseButton", SendMessageOptions.DontRequireReceiver);
+				push = false;
+			} else {
+				Vector2 mousepos = InputManager.Instance.getMousePosMult ();
+				Vector2 mypos = transform.position;
+				Vector2 scl = transform.localScale;
+				if (mypos.x - scl.x / 2 > mousepos.x || mypos.x + scl.x / 2 < mousepos.x || mypos.y - scl.y / 2 > mousepos.y || mypos.y + scl.y / 2 < mousepos.y) {
+					SendMessage ("OnOutButton", SendMessageOptions.DontRequireReceiver);
+					push = false;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : SingletonMonoBehaviour<InputManager> {
+
+	int Click = 0;
+	Vector2 MousePos = new Vector2();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android) {
+			// android
+			clickAndroid ();
+		} else {
+			// pc
+			clickPC ();
+			retryPC ();
+		}
+	}
+
+	void clickAndroid(){
+		if (Input.touchCount > 0) {
+			if (Click == 0) {
+				Click = 1;
+			} else if (Click == 1) {
+				Click = 2;
+			}
+			MousePos = Input.touches [0].position;
+		} else {
+			Click = 0;
+		}
+	}
+
+	void clickPC(){
+		if (Input.GetKey (KeyCode.Mouse0) || Input.GetKey(KeyCode.Space)) {
+			if (Click == 0) {
+				Click = 1;
+			} else if (Click == 1) {
+				Click = 2;
+			}
+			MousePos = Input.mousePosition;
+		} else {
+			Click = 0;
+		}
+	}
+
+	public int getClick(){
+		return Click;
+	}
+
+	void retryPC(){
+		if (Input.GetKeyDown (KeyCode.R)) {
+			goRetry ();
+		}
+	}
+
+	void goRetry(){
+		Application.LoadLevel ("Main");
+	}
+
+
+	public void setRetry(){
+		goRetry ();
+	}
+
+	public Vector2 getMousePos(){
+		return MousePos;
+	}
+
+	public Vector2 getMousePosMult(){
+		Vector2 pos = new Vector2 ();
+		pos.x = MousePos.x / Screen.width;
+		pos.y = MousePos.y / Screen.height;
+		return pos;
+	}
+}
